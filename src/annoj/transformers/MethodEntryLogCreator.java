56d6a922b8bb5fcf8c136214563b2d54ea4bb010
akostajti <akostajti>
2008-11-10 12:11:44 +0000

    system.out.println changed to log4j.getlogger().info

--- src/annoj/transformers/MethodEntryLogCreator.java
+++ src/annoj/transformers/MethodEntryLogCreator.java
@@ -2,21 +2,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package annoj.transformers;
 
 import annoj.annotation.Loggable;
-import java.util.List;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 
 /**
- * Class transformer to create logs on every method invocation.
- *
+ * Class transformer to create logs on every method invocation. For logging it uses
+ * log4j so it can be configured via log4j.properties. IMPORTANT: log4j-*.jar and log4j.properties
+ * have to be on the classpath.
+ * <br/>
+ * Every method is logged on info level.
+ * 
  * @author Tajti Ákos
  */
-public class MethodEntryLogCreator extends ClassTransformer{
+public class MethodEntryLogCreator extends ClassTransformer {
 
     @Override
     protected void doModification(String className) throws Exception {
@@ -25,14 +27,14 @@ public class MethodEntryLogCreator extends ClassTransformer{
         CtMethod[] methods = ct.getDeclaredMethods();
 
         boolean logAll = containsAnnotation(ct.getAnnotations(), Loggable.class);
-        
+
         for (CtMethod method : methods) {
             if (logAll || containsAnnotation(method.getAnnotations(), Loggable.class)) {
-                method.insertBefore("{System.out.println(\"invoked: " + ct.getName() + "." + method.getName() + "(\" + java.util.Arrays.asList($args) +\")\");}");
+                String sd = "{org.apache.log4j.Logger.getLogger(\"" + ct.getName() + "\").info(\"invoked: " + ct.getName() + "." + method.getName() + "(\" + java.util.Arrays.asList($args) +\")\");}";
+                method.insertBefore(sd);
             }
         }
-        
+
         commitClassChanges(ct);
     }
-
 }
