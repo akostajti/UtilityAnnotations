cfebc07887c89e047b51c85bab766468fac7171b
akostajti <akostajti>
2008-11-13 12:39:58 +0000

    extended logging facitlities. now doesnt use log4j

--- src/annoj/annotation/Loggable.java
+++ src/annoj/annotation/Loggable.java
@@ -16,11 +16,36 @@ import java.lang.annotation.Target;
  * <br/>
  * Details: <code>MethodEntryLogCreator</code> inserts a loging method call in the
  * first line of every method to log.
- * 
+ * <br/>
+ * The annotation has one parameter: <code>methods</code>. If the paramter is present, it
+ * contains the method names that must be logged. If not present, then all methods will be logged.
+ * <br/>
+ * It <code>@Loggable</code> is used before a method name, then the method will be logged.
+ * Example:<br/>
+ * <pre>
+ *      @Loggable(methods = {"getSomething"})
+ *      public class C{
+ *          ...
+ *      }
+ * </pre>
+ * In this example the invocation of method <code>getSomething</code> in class <code>C</code>
+ * will be logged.<br/>
+ * <pre>
+*      public class C{
+ *          @Loggable
+ *          public void getSomething(){
+ *          }
+ *      }
+ * </pre>
+ * <br/>
+ * In the above case only <code>getSomething</code> will be logged.
+ * <br/>
+ * Not only public methods can be made loggable - unless the security manager is
+ * configured otherwise.
  * @author tajti
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Loggable {
-
+    String[] methods() default "";
 }
